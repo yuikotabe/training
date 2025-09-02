5fa2c20e59c36a86c38f1125544675f4a4584d36
HW_PC176 <hw_pc176@HW-PC176noMacBook-Air.local>
2025-09-02 12:46:06 +0900

    Vue1回目提出

---
+++ vue01/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type User = {
+  name: string
+  isAdmin: boolean
+}
+
+const user = ref<User>({
+  name: '山田太郎',
+  isAdmin: false
+});
+</script>
+
+<template>
+  <!-- 1問目 -->
+  <p>ユーザー名:{{ user.name }}</p>
+  <p>権限:{{ user.isAdmin? '管理者':'一般'}}</p>
+  
+  <!-- 2問目 -->
+  <button :disabled="!user.isAdmin">編集</button>
+</template>
