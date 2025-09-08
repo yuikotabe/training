ab55febdb6254e676c4e640006b5fcde1e91fff8
HW_PC176 <hw_pc176@HW-PC176noMacBook-Air.local>
2025-09-08 10:19:57 +0900

    Vue課題11回目提出

---
+++ vue11/src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="child-component">
+        <p>子コンポーネント</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onUnmounted } from 'vue'
+onUnmounted(() => {
+  console.log('子コンポーネントが削除されました。')
+})
+</script>
+
+<style scoped>
+.child-component{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 50px;
+    border-radius: 4px;
+    background-color: #20f776;
+}
+</style>
